59997d17ea7c15d1c2012409cdab69922eda280f
ringo2455 <iby2455@gmail.com>
2020-06-14 19:04:11 +0900

    Added Day 8 codes

---
+++ Day8_FunWithHTML5Canvas/index.js
@@ -0,0 +1,39 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+console.log(ctx);
+ctx.strokeStyle = '#BADA55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.globalCompositeOperation = 'darken';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0; // initial is red
+let direction = true;
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.beginPath();
+  // start from
+  ctx.moveTo(lastX, lastY);
+  // go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  hue++;
+  if (hue >= 360) hue = 0;
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) direction = !direction;
+  direction ? ctx.lineWidth++ : ctx.lineWidth--;
+}
+
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', () => (isDrawing = false));
+canvas.addEventListener('mouseout', () => (isDrawing = false));
